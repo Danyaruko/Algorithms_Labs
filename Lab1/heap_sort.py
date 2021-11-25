718d1c8fbbe32a4f4c682e2e718f44f4af9e43ab
Danyaruko <danilpatana1905@gmail.com>
2021-11-25 09:49:51 +0200

    Update heap_sort.py

--- Lab1/heap_sort.py
+++ Lab1/heap_sort.py
@@ -1,11 +1,10 @@
 import time
 
 
-
-def heapify_max(array, size, i): 
-    max_node = i
-    left_child = 2 * i + 1
-    right_child = 2 * i + 2
+def heapify_max(array, size, parent_node):
+    max_node = parent_node
+    left_child = 2 * parent_node + 1
+    right_child = 2 * parent_node + 2
 
     swaps_counter = 0
     comparisons_counter = 0
@@ -22,19 +21,21 @@ def heapify_max(array, size, i):
         comparisons_counter += 1
     comparisons_counter += 1
 
-    if max_node != i:
-        array[max_node], array[i] = array[i], array[max_node]
+    if max_node != parent_node:
+        array[max_node], array[parent_node] = array[parent_node], array[max_node]
         swaps_counter += 1
-        swaps_counter_increment, comparisons_counter_increment = heapify_max(array, size, max_node)
+        swaps_counter_increment, comparisons_counter_increment = heapify_max(
+            array, size, max_node)
         swaps_counter += swaps_counter_increment
         comparisons_counter += comparisons_counter_increment
 
     return swaps_counter, comparisons_counter
 
-def heapify_min(array, size, i): 
-    min_node = i
-    left_child = 2 * i + 1
-    right_child = 2 * i + 2
+
+def heapify_min(array, size, parent_node):
+    min_node = parent_node
+    left_child = 2 * parent_node + 1
+    right_child = 2 * parent_node + 2
 
     swaps_counter = 0
     comparisons_counter = 0
@@ -51,52 +52,56 @@ def heapify_min(array, size, i):
         comparisons_counter += 1
     comparisons_counter += 1
 
-    if min_node != i:
-        array[min_node], array[i] = array[i], array[min_node]
+    if min_node != parent_node:
+        array[min_node], array[parent_node] = array[parent_node], array[min_node]
         swaps_counter += 1
-        swaps_counter_increment, comparisons_counter_increment = heapify_min(array, size, min_node)
+        swaps_counter_increment, comparisons_counter_increment = heapify_min(
+            array, size, min_node)
         swaps_counter += swaps_counter_increment
         comparisons_counter += comparisons_counter_increment
 
-
     return swaps_counter, comparisons_counter
 
-def heap_sort(array, sort_order = "asc"):
+
+def heap_sort(array, sort_order="asc"):
     start_time = time.perf_counter_ns()
     swaps_counter = 0
     comparisons_counter = 0
 
     size = len(array)
-    last_parent_node = size // 2 -1
+    last_parent_node = size // 2 - 1
 
     if sort_order == "asc":
         for node in range(last_parent_node, -1, -1):
             heapify_max(array, size, node)
-        
+
         for node in range(size - 1, 0, -1):
             array[node], array[0] = array[0], array[node]
             swaps_counter += 1
-            swaps_counter_increment, comparisons_counter_increment = heapify_max(array, node, 0)
+            swaps_counter_increment, comparisons_counter_increment = heapify_max(
+                array, node, 0)
             swaps_counter += swaps_counter_increment
             comparisons_counter += comparisons_counter_increment
 
     elif sort_order == "desc":
         for node in range(last_parent_node, -1, -1):
-            swaps_counter_increment, comparisons_counter_increment = heapify_min(array, size, node)
+            swaps_counter_increment, comparisons_counter_increment = heapify_min(
+                array, size, node)
             swaps_counter += swaps_counter_increment
             comparisons_counter += comparisons_counter_increment
 
         for node in range(size - 1, 0, -1):
             array[node], array[0] = array[0], array[node]
             swaps_counter += 1
-            swaps_counter_increment, comparisons_counter_increment = heapify_min(array, node, 0)
+            swaps_counter_increment, comparisons_counter_increment = heapify_min(
+                array, node, 0)
             swaps_counter += swaps_counter_increment
             comparisons_counter += comparisons_counter_increment
 
     else:
         print("Second arg must be one of these: asc, desc")
         return
-    
+
     execution_time = time.perf_counter_ns() - start_time
 
     print("Heap sort:")
@@ -106,7 +111,3 @@ def heap_sort(array, sort_order = "asc"):
     print(array)
 
     return array
-
-
-
-
