93dfb7ed9bfa4c981c622b643c19d7291f16f6ed
Danyaruko <danilpatana1905@gmail.com>
2022-02-03 09:38:12 +0200

    Fix kmp, improve naming

--- Lab7/kmp_algorithm.py
+++ Lab7/kmp_algorithm.py
@@ -2,13 +2,13 @@
 def kmp_algorithm(string, substring):
     def prefix_function(substring):
         prefix_function_res = [0] * len(substring)
-        for curr_string_idx in range(1, len(substring)):
-            curr_substring_idx = prefix_function_res[curr_string_idx - 1]
-            while curr_substring_idx > 0 and substring[curr_substring_idx] != substring[curr_string_idx]:
-                curr_substring_idx = prefix_function_res[curr_substring_idx - 1]
-            if substring[curr_substring_idx] == substring[curr_string_idx]:
-                curr_substring_idx += 1
-            prefix_function_res[curr_string_idx] = curr_substring_idx
+        for curr_substring_idx in range(1, len(substring)):
+            curr_prefix_idx = prefix_function_res[curr_substring_idx - 1]
+            while curr_prefix_idx > 0 and substring[curr_prefix_idx] != substring[curr_substring_idx]:
+                curr_prefix_idx = prefix_function_res[curr_prefix_idx - 1]
+            if substring[curr_prefix_idx] == substring[curr_substring_idx]:
+                curr_prefix_idx += 1
+            prefix_function_res[curr_substring_idx] = curr_prefix_idx
         return prefix_function_res
 
     prefix_function_res = prefix_function(substring)
@@ -17,7 +17,7 @@ def kmp_algorithm(string, substring):
 
     for curr_string_idx in range(len(string)):
         while curr_substring_idx > 0 and string[curr_string_idx] != substring[curr_substring_idx]:
-            curr_substring_idx = prefix_function_res[curr_substring_idx]
+            curr_substring_idx = prefix_function_res[curr_substring_idx - 1]
         if string[curr_string_idx] == substring[curr_substring_idx]:
             curr_substring_idx = curr_substring_idx + 1
         if curr_substring_idx == len(substring):
@@ -27,6 +27,6 @@ def kmp_algorithm(string, substring):
     return kmp_res
 
 
-test_string = "andufaladoraandu"
-test_substring = "an"
+test_string = "ABABABCABABACBCABABABC"
+test_substring = "ABABAC"
 print(kmp_algorithm(test_string, test_substring))
